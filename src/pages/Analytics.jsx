6b37a00046bc017aa55c5862afdd653f4bc57431
Amit jha <amitjha@Amits-MacBook-Pro.local>
2024-11-17 00:13:57 +0530

    Itegrated dark mode

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -3,7 +3,7 @@ import WorkInProgress from "../components/WorkInProgress";
 
 const Analytics = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center">
+    <div className="flex flex-col items-center justify-center h-full text-center min-h-[105vh]">
       <h1 className="text-4xl font-bold text-gray-800">Analytics</h1>
       <WorkInProgress
         alt="Analytics Work in Progress"
